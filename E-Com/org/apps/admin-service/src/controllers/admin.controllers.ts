ba81391b62b132a30b68c86a3ebd4ecead53bc4c
Harsh Kumar <hemenderkumar3000@gmail.com>
2025-07-27 02:09:55 +0530

    Admin Routes

--- E-Com/org/apps/admin-service/src/controllers/admin.controllers.ts
+++ E-Com/org/apps/admin-service/src/controllers/admin.controllers.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from "@packages/error-handler"
 import prisma from "@packages/libs/prisma"
 import { Prisma } from "@prisma/client"
 import { NextFunction, Request, Response } from "express"
@@ -97,6 +98,115 @@ export const getAllAdmins=async (req:Request,res:Response,next:NextFunction)=> {
         res.status(201).json({
             success: true,
             admins,
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+export const addNewAdmin=async (req:Request,res:Response,next:NextFunction)=> {
+    try {
+        const {email,role}=req.body
+        const isUser = await prisma.users.findUnique({ where: { email } })
+        if (!isUser) return next(new ValidationError("Something went wrong!"))
+        const updateRole = await prisma.users.update({
+            where: { email },
+            data: {
+            role
+        }})
+        res.status(201).json({
+            success: true,
+            updateRole
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+export const getAllCustomizations=async (req:Request,res:Response,next:NextFunction)=> {
+    try {
+        const config = await prisma.site_config.findFirst()
+
+        res.status(201).json({
+            success: true,
+            categories: config?.categories || [],
+            subCategories: config?.subCategories || [],
+            logo: config?.logo || null,
+            banner:config?.banner||null
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+
+export const getAllUsers=async (req:Request,res:Response,next:NextFunction)=> {
+    try {
+        const page = parseInt(req.query.page as string) || 1
+        const limit = parseInt(req.query.limit as string) || 20
+        const skip = (page - 1) * limit
+        const [users, total] = await Promise.all([prisma.users.findMany({
+            skip,
+            take: limit,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                avatar: true,
+                createdAt: true,
+            },
+            orderBy:{createdAt:"desc"},
+        }),
+            prisma.users.count()
+        ])
+        res.status(201).json({
+            success: true,
+            data: users,
+            meta: {
+            totalUsers:total,
+            currentPage: page,
+            totalPages:Math.ceil(total/limit)
+            }
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+export const getAllSellers=async (req:Request,res:Response,next:NextFunction)=> {
+    try {
+        const page = parseInt(req.query.page as string) || 1
+        const limit = parseInt(req.query.limit as string) || 20
+        const skip = (page - 1) * limit
+        const [sellers, total] = await Promise.all([prisma.sellers.findMany({
+            skip,
+            take: limit,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                createdAt: true,
+                shop: {
+                    select: {
+                        name: true,
+                        avatar: true,
+                        address:true
+                    }
+                }
+            },
+            orderBy:{createdAt:"desc"},
+        }),
+            prisma.sellers.count()
+        ])
+        res.status(201).json({
+            success: true,
+            data: sellers,
+            meta: {
+            totalSellers:total,
+            currentPage: page,
+            totalPages:Math.ceil(total/limit)
+            }
         })
     } catch (error) {
         return next(error)
--- E-Com/org/apps/admin-service/src/routes/admin.route.ts
+++ E-Com/org/apps/admin-service/src/routes/admin.route.ts
@@ -1,10 +1,14 @@
 import express,{ Router } from "express";
-import { getAllProducts } from "../controllers/admin.controllers";
+import { addNewAdmin, getAllAdmins, getAllCustomizations, getAllEvents, getAllProducts } from "../controllers/admin.controllers";
 import isAuthenticated from "@packages/middleware/isAuthenticated";
 import { isAdmin } from "@packages/middleware/authorizeRole";
 
 const router: Router = express.Router()
 
 router.get("/get-all-products",isAuthenticated,isAdmin ,getAllProducts)
+router.get("/get-all-events",isAuthenticated,isAdmin ,getAllEvents)
+router.get("/get-all-admins",isAuthenticated,isAdmin ,getAllAdmins)
+router.put("/add-new-admin",isAuthenticated,isAdmin ,addNewAdmin)
+router.get("/get-all" ,getAllCustomizations)
 
 export default router
