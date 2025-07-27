ba82720bea9f5dad86e442901b5a609d5a6e063d
Harsh Kumar <hemenderkumar3000@gmail.com>
2025-07-28 00:58:49 +0530

    Seller WebSocket setup

--- E-Com/org/apps/seller-ui/src/app/providers.tsx
+++ E-Com/org/apps/seller-ui/src/app/providers.tsx
@@ -1,25 +1,41 @@
 'use client'
 import React, { useState } from 'react'
 import { Query, QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import {Toaster} from "sonner"
+import { Toaster } from "sonner"
+import useSeller from '../hooks/useSeller'
+import { WebSocketProvider } from '../context/web-socket-context'
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient({
     defaultOptions: {
       queries: {
         refetchOnWindowFocus: false,
-        staleTime:1000*60*5
-        }
+        staleTime: 1000 * 60 * 5
       }
-    }))
+    }
+  }))
   return (
-      <div>
-          <QueryClientProvider client={queryClient}>
-        {children}
-        <Toaster/>
-          </QueryClientProvider>
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <ProviderWithWebSocket>
+          {children}
+        </ProviderWithWebSocket>
+        <Toaster />
+      </QueryClientProvider>
     </div>
   )
+}
+
+const ProviderWithWebSocket = ({ children }: { children: React.ReactNode }) => {
+  const { seller, isLoading } = useSeller()
+  if (isLoading) return null
+  return (<>{seller &&
+    <WebSocketProvider seller={seller}>
+      {children}
+    </WebSocketProvider>
+  }
+    {!seller && children}
+  </>)
 }
 
 export default Providers
